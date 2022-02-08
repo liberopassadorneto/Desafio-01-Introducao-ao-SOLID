48e76408d4b4a618a914ef274a7e973bc8cce36b
libero <lneto@tecnologia-unica.com.br>
2022-02-07 20:24:34 -0400

    first commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found!");
+    } else if (user.admin === false) {
+      throw new Error("You are not an admin!");
+    }
+
+    const allUsers = this.usersRepository.list();
+    return allUsers;
+  }
+}
+
+export { ListAllUsersUseCase };
---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowUserProfileUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found!");
+    }
+
+    this.usersRepository.list();
+
+    return user;
+  }
+}
+
+export { ShowUserProfileUseCase };
